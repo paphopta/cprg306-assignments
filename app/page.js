435cc7a01dd2e56bdfeffb8a3184274d9323fb6d
paphopta <paphop.ta@gmail.com>
2025-09-16 22:43:12 -0600

    Add week 2 assignment and move week 1 assignment to app

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <p>CPRG 306: Web Development 2 - Assignments</p>
+        <p><Link href="./week-2">Go to Week 2</Link></p>
+      </main>
+    </div>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export function StudentInfo() {
+  return (<div>
+            <p>Name: Paphop Manakamjorn</p>
+            <p>Github: <Link href="https://github.com/paphopta/cprg306-assignments">https://github.com/paphopta/cprg306-assignments</Link></p>
+          </div>);
+}
+
+export default StudentInfo;
